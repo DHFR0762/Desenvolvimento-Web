bd1c3cad0f5929a6e5f56de9a4456ed93103781b
DHFR0762 <dhfr0762@gmail.com>
2026-09-04 10:02:14 -0300

    Update Main.java

--- Aula Java 1/Main.java
+++ Aula Java 1/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
   public static void main(String[] args){
 
-    System.out.println("Sistema de Projetos"); 
+    System.out.println("============================="); 
+    System.out.println("     SISTEMA DE PROJETOS");
+    System.out.println("============================="); 
+    System.out.println("Bem-vindo ao sistema!");  
   }
 }
